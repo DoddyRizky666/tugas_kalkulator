5fbe649138c17ff6b3fa26f7f2dab505cffe1c73
DoddyRizky666 <149789959+DoddyRizky666@users.noreply.github.com>
2024-01-04 18:39:03 +0700

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,24 @@
+function tambah() {
+var angka1 = parseFloat(document.angka.angka1.value);
+var angka2 = parseFloat(document.angka.angka2.value);
+var jumlah =angka1+angka2;
+alert ("hasilnya adalah "+jumlah);
+} 
+function kurang() {
+    var angka1 = parseFloat(document.angka.angka1.value);
+    var angka2 = parseFloat(document.angka.angka2.value);
+    var jumlah =angka1-angka2;
+    alert ("hasilnya adalah "+jumlah);
+    } 
+    function kali() {
+        var angka1 = parseFloat(document.angka.angka1.value);
+        var angka2 = parseFloat(document.angka.angka2.value);
+        var jumlah =angka1*angka2;
+        alert ("hasilnya adalah "+jumlah);
+        } 
+        function bagi() {
+            var angka1 = parseFloat(document.angka.angka1.value);
+            var angka2 = parseFloat(document.angka.angka2.value);
+            var jumlah =angka1/angka2;
+            alert ("hasilnya adalah "+jumlah);
+            } 
